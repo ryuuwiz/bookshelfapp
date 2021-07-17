f3911d0c1740ce6f9336e930df03ba0ec38df8d4
ryuuwiz <ryuuwiz@gmail.com>
2021-07-17 19:35:14 +0700

    feat: add version on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="p-3 m-3 text-center">
     <p class="mb-2">Simple Bookshelf App by ryuuwiz</p>
+    <p class="mb-2">Version {{ version }}</p>
     <a href="https://github.com/ryuuwiz" target="_blank">
       <i class="fab fa-github-alt fa-2x cursor-pointer"></i>
     </a>
@@ -8,7 +9,13 @@
 </template>
 
 <script>
+import { version } from '../../package.json'
 export default {
   name: 'Footer',
+  setup() {
+    return {
+      version,
+    }
+  },
 }
 </script>
