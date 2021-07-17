8e3de36df7b83fc8bfc831b19da2d59a0a78a31c
ryuuwiz <ryuuwiz@gmail.com>
2021-07-17 18:49:35 +0700

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,25 @@
       <Search />
       <Bookshelf />
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import Form from './components/Form.vue'
-import Bookshelf from './components/Bookshelf.vue'
 import Search from './components/Search.vue'
+import Bookshelf from './components/Bookshelf.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
     Form,
-    Search,
     Bookshelf,
+    Search,
+    Footer,
   },
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-3 m-3 text-center">
+    <p class="mb-2">Simple Bookshelf App by ryuuwiz</p>
+    <a href="https://github.com/ryuuwiz" target="_blank">
+      <i class="fab fa-github-alt fa-2x cursor-pointer"></i>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
