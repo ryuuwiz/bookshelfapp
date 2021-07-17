db3ecca7f25814a98128e35d3fbc5e78e5591e97
ryuuwiz <ryuuwiz@gmail.com>
2021-07-17 18:40:29 +0700

    style: add icon to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,10 @@
 <template>
   <header
-    class="p-3 mb-5 shadow-xl bg-gradient-to-r from-indigo-400 to-blue-500"
+    class="p-4 mb-5 shadow-xl bg-gradient-to-r from-indigo-400 to-blue-500"
   >
-    <h1 class="text-3xl text-center font-bold text-white">Bookshelf App</h1>
+    <h1 class="text-3xl text-center font-bold text-white">
+      <i class="fas fa-book fa-lg mr-2"></i> Bookshelf App
+    </h1>
   </header>
 </template>
 
