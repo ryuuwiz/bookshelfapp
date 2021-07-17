daea83eed214c93fbe968c0955ad79d251ae97d8
ryuuwiz <ryuuwiz@gmail.com>
2021-07-17 15:06:47 +0700

    feat: can store data in localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,109 @@
 import { createStore, createLogger } from 'vuex'
-import bookshelf from './modules/bookshelf'
+import { nanoid } from 'nanoid'
+import createPersistedState from 'vuex-persistedstate'
+import SecureLS from 'secure-ls'
 
 const debug = process.env.NODE_ENV !== 'production'
+const ls = new SecureLS({ isCompression: false })
 
 export default createStore({
-  modules: {
-    bookshelf,
+  // State
+  state: () => ({
+    bookshelf: [
+      {
+        id: nanoid(),
+        title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+        author: 'Marijn Haverbeke',
+        year: 2011,
+        isComplete: false,
+      },
+      {
+        id: nanoid(),
+        title: 'Fullstack Vue: The Complete Guide to Vue.js and Friends',
+        author: 'Ari Lerner and Nate Murray',
+        year: 2018,
+        isComplete: true,
+      },
+    ],
+    search: {
+      query: '',
+      status: false,
+    },
+  }),
+
+  // Actions
+  actions: {
+    addBook: async ({ commit }, data) => await commit('setBook', data),
+
+    removeBook: async ({ commit }, data) => await commit('dropBook', data),
+
+    addBookToComplete: async ({ commit }, data) =>
+      await commit('setBookToComplete', data),
+
+    addBookToInComplete: async ({ commit }, data) =>
+      await commit('setBookToInComplete', data),
+
+    addSearch: async ({ commit }, data) => await commit('setSearch', data),
+  },
+
+  // Mutations
+  mutations: {
+    setBook: (state, payload) =>
+      state.bookshelf.push({
+        id: nanoid(),
+        title: payload.bookTitle,
+        author: payload.bookYear,
+        year: payload.bookYear,
+        isComplete: payload.bookisComplete,
+      }),
+
+    dropBook: (state, payload) => state.bookshelf.splice(payload, 1),
+
+    setBookToComplete: (state, payload) =>
+      (state.bookshelf[payload].isComplete = true),
+
+    setBookToInComplete: (state, payload) =>
+      (state.bookshelf[payload].isComplete = false),
+
+    setSearch: (state, payload) => (state.search = payload),
+  },
+
+  // Getters
+  getters: {
+    getBookshelf: (state) => state.bookshelf,
+
+    inCompleteBooks: (state) =>
+      state.bookshelf.filter((e) => e.isComplete === false),
+
+    completeBooks: (state) =>
+      state.bookshelf.filter((e) => e.isComplete === true),
+
+    findBookIndex: (state) => (id) => {
+      let i = 0
+      for (let book of state.bookshelf) {
+        if (book.id === id) return i
+        i++
+      }
+      return -1
+    },
+
+    getSearchStatus: (state) => state.search.status,
+
+    findBookTitle: (state) =>
+      state.bookshelf.filter((el) =>
+        new RegExp(state.search.query, 'gi').test(el.title),
+      )[0].title,
   },
   strict: debug,
-  plugins: debug ? [createLogger()] : [],
+  plugins: debug
+    ? [createPersistedState(), createLogger()]
+    : [
+        createPersistedState({
+          storage: {
+            getItem: (key) => ls.get(key),
+            setItem: (key, value) => ls.set(key, value),
+            removeItem: (key) => ls.remove(key),
+          },
+        }),
+      ],
 })
